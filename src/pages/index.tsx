67295826660ac05ec42b6022085e25a64c8d8049
korni <kornelussegg12@gmail.com>
2022-10-10 17:23:37 +0200

    + only player with phone resolution

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import MainLayout, { MainContent } from '@/layouts/MainLayout';
 import Dashboard from '@/domain/Dashboard/view/Dashboard';
 import Head from 'next/head';
 import type { NextPage } from 'next';
+import { Hidden } from '@mui/material';
 import SimpleBar from 'simplebar-react';
 import TwitchChat from '@/domain/TwitchChat/view/TwitchChat';
 import VideoPlayer from '@/domain/VideoPlayer/view/VideoPlayer';
@@ -24,20 +25,30 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <MainLayout>
+        <Hidden lgDown>
       <HorizontalPageSplit onResizeMove={setPointerActive} onResizeEnd={setPointerRemove} >
+
       <div style={{minWidth: '57%' }} >
         <MainContent>
-          <SimpleBar style={{maxHeight: '100vh', zIndex: '100',
-                              background: 'radial-gradient(circle, rgba(60, 60, 62, 1) 0%, rgba(23, 25, 31, 1) 100%)' }}>
+          <SimpleBar style={{ maxHeight: '100vh', zIndex: '100',
+                              background: 'radial-gradient(circle, rgba(60, 60, 62, 1) 0%, rgba(23, 25, 31, 1) 100%)', }}>
             <VideoPlayer />
             <Dashboard />
           </SimpleBar>
         </MainContent>
         </div>
-        <div style={{minWidth: '15%', maxWidth:'36%'}}>
-          <TwitchChat />
-        </div>
-        </HorizontalPageSplit>
+
+              <div style={{minWidth: '15%', maxWidth:'36%'}}>
+                <TwitchChat />
+              </div>
+              
+          </HorizontalPageSplit>
+          </Hidden>
+
+          <Hidden lgUp>
+          <VideoPlayer/>
+          </Hidden>
+
       </MainLayout>
     </>
   );
