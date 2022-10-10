59f3f38ea59bd44b4f9246a9fd6f3fb941ae7a9c
korni <kornelussegg12@gmail.com>
2022-10-10 14:52:55 +0200

    fix videocard
    fix videocard

--- src/components/VideoCard/VideoCard.tsx
+++ src/components/VideoCard/VideoCard.tsx
@@ -1,24 +1,30 @@
-import { EmptyVideoCard, VideoBox, VideoCardWrapper, VideoContent } from './VideoCard.styles';
+import { EmptyVideoCard, VideoBox, VideoCardWrapper, VideoContent, } from './VideoCard.styles';
 import { Link, Typography } from '@mui/material';
 import React, { FC } from 'react';
 
 import { PlaylistWithUsers } from '@/domain/Playlist/model/Playlist.model';
 import VideoThumbnail from '@/components/VideoThumbnail';
 import { useTranslation } from 'react-i18next';
+import { AddedByAvatar, AddedByWrapper } from '@/styles/style'
+import { usePlayerContext } from '@/domain/VideoPlayer/context/PlayerContext';
 
+import { Seeker }  from '@/styles/style'
 interface VideoCardProps {
   video: PlaylistWithUsers | undefined;
 }
 
 const VideoCard: FC<VideoCardProps> = ({ video }) => {
   const { t } = useTranslation();
+  const {playerState } = usePlayerContext();
+  const { playedSeconds, duration, activeVideo } = playerState;
+
 
   if (!video)
     return (
       <VideoCardWrapper elevation={0}>
         <EmptyVideoCard>
           <Typography variant='h3'>{t('playlist.noCurrentVideo.title')}</Typography>
-          <Typography variant='body2'>{t('playlist.noCurrentVideo.subtitle')}</Typography>
+          <Typography variant='body2' style={{cursor: 'default'}} >{t('playlist.noCurrentVideo.subtitle')}</Typography>
         </EmptyVideoCard>
       </VideoCardWrapper>
     );
@@ -31,14 +37,23 @@ const VideoCard: FC<VideoCardProps> = ({ video }) => {
       </Link>
       <VideoBox>
         <VideoContent>
-          <Typography noWrap variant='h2' sx={{ width: '98%' }}>
-            <Link href={videoUrl} target='_blank' rel='noopener norefferer'>
+          <Link href={videoUrl} target='_blank' rel='noopener norefferer' sx={{ width: '98%' }}>
+            <Typography noWrap variant='h2'>
               {videoTitle}
-            </Link>
-          </Typography>
-          <Typography variant='body1'>
-            {t('video.addedBy')}: {addedBy.name}
-          </Typography>
+            </Typography>
+          </Link>
+          <Seeker
+        aria-label='time-indicator'
+        size='small'
+        value={activeVideo ? playedSeconds : 0}
+        min={0}
+        step={1}
+        max={activeVideo ? duration : 1}
+      />
+          <AddedByWrapper>
+          {addedBy.image ? <AddedByAvatar variant='square' src={addedBy.image} /> : null}
+          <Typography component='span' style={{cursor: 'default'}} >{addedBy.name}</Typography>
+        </AddedByWrapper>
         </VideoContent>
       </VideoBox>
     </VideoCardWrapper>
