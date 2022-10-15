53c46d4bcbb527c5ad807176b61ce982b3a26408
korni <kornelussegg12@gmail.com>
2022-10-15 13:51:53 +0200

    tooltip fix

--- src/domain/Navigation/Navigation.tsx
+++ src/domain/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import { Box } from '@mui/material';
+import { Box, Tooltip } from '@mui/material';
 import Image from 'next/image';
 import Twitch from '@/domain/Icons/Twitch.svg'
 import Discord from '@/domain/Icons/Discord.svg'
@@ -13,17 +13,20 @@ const Navigation = () => {
 
   return (
     <Box sx={{ width: '100%', height: '56px', display: 'flex', position: 'absolute' , gap: '150px', justifyContent: 'center', overflow: 'hidden', background: 'rgba(0, 0, 0, 0.50)'}}>
-
 <a href='https://www.twitch.tv/khamires' >
+<Tooltip title={t('playlist.tooltip.twitch')}>
         <TwitchBox>
         <Image src={Twitch} alt='Twitch' />
-        </TwitchBox></a>
+        </TwitchBox>
+        </Tooltip>
+        </a>
 <NavText>{t('Navigation.twitch')}</NavText>
-
 <a href='https://discord.gg/Vu8VFS4wZ9'>
+<Tooltip title={t('playlist.tooltip.discord')}>
         <DiscordBox>
         <Image src={Discord} alt='Discord' />
-        </DiscordBox></a>
+        </DiscordBox>
+        </Tooltip></a>
 <NavText>{t('Navigation.discord')}</NavText>
 
 
