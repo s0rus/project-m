622c90888f6e485bb208f95987921a911ea29300
s0rus <piotrmolkontakt@gmail.com>
2022-10-21 13:35:50 +0200

    Test deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { env } from './src/env/server.mjs';
-
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
@@ -18,4 +16,5 @@ export default defineNextConfig({
   images: {
     domains: ['img.youtube.com'],
   },
+  output: 'standalone',
 });
