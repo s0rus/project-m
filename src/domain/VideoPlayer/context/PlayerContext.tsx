944400bf7f65d1364c24f68bb0f3b86acf4de74b
korni <kornelussegg12@gmail.com>
2022-11-14 21:20:05 +0100

    .

--- src/domain/VideoPlayer/context/PlayerContext.tsx
+++ src/domain/VideoPlayer/context/PlayerContext.tsx
@@ -28,26 +28,14 @@ export const PlayerContextProvider: FC<PropsWithChildren> = ({ children }) => {
   const getDuration = useCallback(() => playerRef?.current?.getDuration() || 0, [playerRef]);
   const getPlayedSeconds = useCallback(() => playerRef?.current?.getCurrentTime() || 0, [playerRef]);
   const isPlayerPlaying = useMemo(() => playerState.isPlaying, [playerState.isPlaying]);
-
-  const resetPlayerState = useCallback(async () => {
-    setPlayerState((prevPlayerState) => {
-      return {
-        ...prevPlayerState,
-        duration: 0,
-        playedSeconds: 0,
-        loadedSeconds: 0,
-        activeVideo: undefined,
-        isReady: false,
-      };
-    });
-  }, []);
-
   const requestPlayerState = useCallback(() => {
-    if (!socket) {
-      return;
-    }
+    if (!socket || !playerState.isReady) return;
     socket.emit('REQUEST_PLAYER_STATE');
-  }, [socket]);
+  }, [socket, playerState.isReady]);
+
+  useEffect(() => {
+    requestPlayerState();
+  }, [requestPlayerState]);
 
   useEffect(() => {
     setPlayerState((prevPlayerState) => {
@@ -77,6 +65,28 @@ export const PlayerContextProvider: FC<PropsWithChildren> = ({ children }) => {
     });
   }, []);
 
+  const toggleMuted = useCallback(() => {
+    setPlayerState((prevPlayerState) => {
+      return {
+        ...prevPlayerState,
+        isMuted: prevPlayerState.isReady && !prevPlayerState.isMuted,
+        initialMute: prevPlayerState.initialMute && prevPlayerState.isReady && false,
+      };
+    });
+  }, []);
+
+  const setVolume = useCallback((_: Event, value: number | number[]) => {
+    setPlayerState((prevPlayerState) => {
+      return {
+        ...prevPlayerState,
+        volume: value as number,
+        isMuted: prevPlayerState.isMuted && false,
+        initialMute: prevPlayerState.initialMute && false,
+      };
+    });
+    localStorage.setItem(LocalStorageKeys.PlayerVolume, JSON.stringify(value as number));
+  }, []);
+
   const handleProgress = useCallback(
     ({ loadedSeconds, playedSeconds }: ProgressProps) => {
       if (!seeking) {
@@ -95,8 +105,8 @@ export const PlayerContextProvider: FC<PropsWithChildren> = ({ children }) => {
 
   const handleSeek = useCallback(
     (newPlayedSeconds: number) => {
-      if (isAdmin && socket) {
-        socket.emit('SEEK_TO', newPlayedSeconds);
+      if (isAdmin) {
+        socket && socket.emit('SEEK_TO', newPlayedSeconds);
         setPlayerState((prevPlayerState) => {
           return {
             ...prevPlayerState,
@@ -117,18 +127,6 @@ export const PlayerContextProvider: FC<PropsWithChildren> = ({ children }) => {
     });
   }, []);
 
-  const setVolume = useCallback((_: Event, value: number | number[]) => {
-    setPlayerState((prevPlayerState) => {
-      return {
-        ...prevPlayerState,
-        volume: value as number,
-        isMuted: prevPlayerState.isMuted && false,
-        initialMute: prevPlayerState.initialMute && false,
-      };
-    });
-    localStorage.setItem(LocalStorageKeys.PlayerVolume, JSON.stringify(value as number));
-  }, []);
-
   const disableInitialMute = useCallback(() => {
     setPlayerState((prevPlayerState) => {
       return {
@@ -139,60 +137,87 @@ export const PlayerContextProvider: FC<PropsWithChildren> = ({ children }) => {
     });
   }, []);
 
-  const toggleMuted = useCallback(() => {
+  const handleOnEnd = useCallback(
+    (targetVideoId?: string) => {
+      console.log(targetVideoId);
+      setPlayerState((prevPlayerState) => {
+        return {
+          ...prevPlayerState,
+          duration: 0,
+          playedSeconds: 0,
+          loadedSeconds: 0,
+          activeVideo: undefined,
+        };
+      });
+
+      requestNextVideo(targetVideoId);
+    },
+    [requestNextVideo]
+  );
+
+  const handleOnVideoSkip = useCallback(() => {
     setPlayerState((prevPlayerState) => {
       return {
         ...prevPlayerState,
-        isMuted: prevPlayerState.isReady ? !prevPlayerState.isMuted : true,
-        initialMute: prevPlayerState.isReady ? !prevPlayerState.initialMute : true,
+        duration: 0,
+        playedSeconds: 0,
+        loadedSeconds: 0,
+        activeVideo: undefined,
+      };
+    });
+
+    handleSkipVideo();
+  }, [handleSkipVideo]);
+
+  const handleOnError = useCallback(() => {
+    CustomToast.send(t('toast.videoError'), ToastTypes.VideoSkipped);
+    setPlayerState((prevPlayerState) => {
+      return {
+        ...prevPlayerState,
+        duration: 0,
+        playedSeconds: 0,
+        loadedSeconds: 0,
+        activeVideo: undefined,
+      };
+    });
+    requestNextVideo();
+  }, [t, requestNextVideo]);
+
+  const handleOnPlayVideoNow = useCallback(() => {
+    setPlayerState((prevPlayerState) => {
+      return {
+        ...prevPlayerState,
+        duration: 0,
+        playedSeconds: 0,
+        loadedSeconds: 0,
+        activeVideo: undefined,
       };
     });
   }, []);
 
-  const handleOnEnd = useCallback(
-    (targetVideoId?: string) => {
-      resetPlayerState();
-      requestNextVideo(targetVideoId);
-    },
-    [requestNextVideo, resetPlayerState]
-  );
-
-  const handleOnVideoSkip = useCallback(() => {
-    resetPlayerState();
-    handleSkipVideo();
-  }, [handleSkipVideo, resetPlayerState]);
-
-  const handleOnError = useCallback(() => {
-    CustomToast.send(t('toast.videoError'), ToastTypes.VideoSkipped);
-    resetPlayerState();
-    requestNextVideo();
-  }, [t, requestNextVideo, resetPlayerState]);
-
-  const handleOnPlayVideoNow = useCallback(() => resetPlayerState(), [resetPlayerState]);
-
   const handleOnReady = useCallback(() => {
-    setPlayerState((prevPlayerState) => {
-      return {
-        ...prevPlayerState,
-        isReady: true,
-      };
-    });
-
-    requestPlayerState();
-  }, [requestPlayerState]);
+    if (!playerState.isReady && socket) {
+      setPlayerState((prevPlayerState) => {
+        return {
+          ...prevPlayerState,
+          isReady: true,
+        };
+      });
+    }
+  }, [playerState.isReady, socket]);
 
   useEffect(() => {
     if (!socket) return;
 
     socket.on('RECEIVE_REQUEST_PLAYER_STATE', (socketId) => {
       const currentPlayedSeconds = getPlayedSeconds() || 0;
-      // const playedSecondsWithDelay = isPlayerPlaying ? currentPlayedSeconds + 1 : currentPlayedSeconds;
+      const playedSecondsWithDelay = isPlayerPlaying ? currentPlayedSeconds + 1 : currentPlayedSeconds;
 
       socket.emit(
         'SEND_PLAYER_STATE',
         {
           isPlaying: isPlayerPlaying,
-          playedSeconds: currentPlayedSeconds,
+          playedSeconds: playedSecondsWithDelay,
         },
         socketId
       );
@@ -207,6 +232,8 @@ export const PlayerContextProvider: FC<PropsWithChildren> = ({ children }) => {
     socket.on('RECEIVE_SKIP_VIDEO', (targetVideoId) => handleOnEnd(targetVideoId));
     socket.on('RECEIVE_PLAYER_STATE', (receivedPlayerState) => {
       if (receivedPlayerState) {
+        console.log(receivedPlayerState);
+
         setPlayerState((prevPlayerState) => {
           return {
             ...prevPlayerState,
@@ -215,10 +242,6 @@ export const PlayerContextProvider: FC<PropsWithChildren> = ({ children }) => {
           };
         });
         seekTo(receivedPlayerState.playedSeconds);
-      } else {
-        setTimeout(() => {
-          requestPlayerState();
-        }, 2000);
       }
     });
 
@@ -228,7 +251,7 @@ export const PlayerContextProvider: FC<PropsWithChildren> = ({ children }) => {
       socket.off('RECEIVE_SKIP_VIDEO');
       socket.off('RECEIVE_PLAYER_STATE');
     };
-  }, [socket, togglePlaying, handleOnEnd, seekTo, requestPlayerState]);
+  }, [socket, togglePlaying, handleOnEnd, seekTo]);
 
   const value = useMemo(
     () => ({
